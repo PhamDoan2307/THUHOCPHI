9c89940cfd750d456b88e73e5f02a02671041842
PhamDoan2307 <yaphetsss.94@gmail.com>
2016-05-04 09:22:00 +0700

    Added files via upload

---
+++ frm_Monhoc.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ThuHocPhi.Views.TuDien
+{
+    partial class frm_Monhoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_capnhat = new System.Windows.Forms.Button();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_chon = new System.Windows.Forms.TextBox();
+            this.btn_chon = new System.Windows.Forms.Button();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_timkiem = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(2, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 262);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(652, 262);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_capnhat
+            // 
+            this.btn_capnhat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_capnhat.Location = new System.Drawing.Point(484, 340);
+            this.btn_capnhat.Name = "btn_capnhat";
+            this.btn_capnhat.Size = new System.Drawing.Size(75, 23);
+            this.btn_capnhat.TabIndex = 2;
+            this.btn_capnhat.Text = "Cập nhật MH";
+            this.btn_capnhat.UseVisualStyleBackColor = true;
+            this.btn_capnhat.Click += new System.EventHandler(this.btn_capnhat_Click);
+            // 
+            // btn_them
+            // 
+            this.btn_them.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_them.Location = new System.Drawing.Point(573, 340);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(75, 23);
+            this.btn_them.TabIndex = 2;
+            this.btn_them.Text = "Thêm MH";
+            this.btn_them.UseVisualStyleBackColor = true;
+            this.btn_them.Click += new System.EventHandler(this.btn_them_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã MH:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chọn file:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_chon
+            // 
+            this.txt_chon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_chon.Location = new System.Drawing.Point(330, 24);
+            this.txt_chon.Name = "txt_chon";
+            this.txt_chon.Size = new System.Drawing.Size(148, 20);
+            this.txt_chon.TabIndex = 1;
+            // 
+            // btn_chon
+            // 
+            this.btn_chon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_chon.Location = new System.Drawing.Point(484, 22);
+            this.btn_chon.Name = "btn_chon";
+            this.btn_chon.Size = new System.Drawing.Size(75, 23);
+            this.btn_chon.TabIndex = 2;
+            this.btn_chon.Text = "Chọn";
+            this.btn_chon.UseVisualStyleBackColor = true;
+            // 
+            // btn_import
+            // 
+            this.btn_import.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_import.Location = new System.Drawing.Point(571, 22);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(75, 23);
+            this.btn_import.TabIndex = 2;
+            this.btn_import.Text = "Import";
+            this.btn_import.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txt_timkiem);
+            this.groupBox1.Controls.Add(this.btn_import);
+            this.groupBox1.Controls.Add(this.btn_chon);
+            this.groupBox1.Controls.Add(this.txt_chon);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(652, 66);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // txt_timkiem
+            // 
+            this.txt_timkiem.Location = new System.Drawing.Point(91, 25);
+            this.txt_timkiem.Name = "txt_timkiem";
+            this.txt_timkiem.Size = new System.Drawing.Size(100, 20);
+            this.txt_timkiem.TabIndex = 3;
+            this.txt_timkiem.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_timkiem_KeyUp);
+            // 
+            // frm_Monhoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 370);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.btn_capnhat);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm_Monhoc";
+            this.Text = "frm_Monhoc";
+            this.Load += new System.EventHandler(this.frm_Monhoc_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_capnhat;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_chon;
+        private System.Windows.Forms.Button btn_chon;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_timkiem;
+    }
+}
---
+++ frm_Monhoc.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThuHocPhi.Controls.Tudien;
+using ThuHocPhi.Models;
+using ThuHocPhi.Models.Tudien;
+using ThuHocPhi.Shares;
+
+namespace ThuHocPhi.Views.TuDien
+{
+    public partial class frm_Monhoc : Form
+    {
+        DataDataContext db = new DataDataContext();
+        monhoc_ctrl mh_ctrl = new monhoc_ctrl();
+
+        public frm_Monhoc()
+        {
+            InitializeComponent();
+            btn_chon.Click += new EventHandler(btn_chon_click);
+            btn_import.Click += new EventHandler(btn_import_click);
+        }
+
+        private void btn_chon_click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            //of.Filter = ".xls||.xlsx";
+            txt_chon.Text = of.ShowDialog() == DialogResult.OK ? of.FileName : "";
+        }
+
+        private void btn_import_click(object sender, EventArgs e)
+        {
+            if (!ValidInput())
+                return;
+            showdata();
+            DataTable data = ReadDataFromExcelFile();
+            ImportIntoDatabase(data);
+            dataGridView1.DataSource = mh_ctrl.GetAllMH().Data;
+        }
+        private void showdata()
+        {
+            DataHocphiTableAdapters.MonHocTableAdapter adapter = new DataHocphiTableAdapters.MonHocTableAdapter();
+            dataGridView1.DataSource = adapter.GetData();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool ValidInput()
+        {
+            if (txt_chon.Text.Trim() == "")
+            {
+                MessageBox.Show("Xin vui lòng chọn tập tin excel cần import");
+                
+                btn_chon.Focus();
+                return false;
+            }
+            return true;
+        }
+        private DataTable ReadDataFromExcelFile()
+        {
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + txt_chon.Text.Trim() + ";Extended Properties=Excel 8.0";
+            // Tạo đối tượng kết nối
+            OleDbConnection oledbConn1 = new OleDbConnection(connectionString);
+            DataTable data = null;
+            try
+            {
+                // Mở kết nối
+                oledbConn1.Open();
+
+                // Tạo đối tượng OleDBCommand và query data từ sheet có tên "Sheet1"
+                 OleDbCommand cmd1 = new OleDbCommand("SELECT * FROM [TDMH$]", oledbConn1);
+
+                // Tạo đối tượng OleDbDataAdapter để thực thi việc query lấy dữ liệu từ tập tin excel
+                OleDbDataAdapter oleda1 = new OleDbDataAdapter();
+
+                oleda1.SelectCommand = cmd1;
+
+                // Tạo đối tượng DataSet để hứng dữ liệu từ tập tin excel
+                DataSet ds = new DataSet();
+
+                // Đổ đữ liệu từ tập excel vào DataSet
+                oleda1.Fill(ds);
+                data = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                // Đóng chuỗi kết nối
+                oledbConn1.Close();
+            }
+            return data;
+           
+        }
+        private void ImportIntoDatabase(DataTable data)
+        {
+            if (data == null || data.Rows.Count == 0)
+            {
+                MessageBox.Show("Không có dữ liệu để import");
+                return;
+            }
+            DataHocphiTableAdapters.MonHocTableAdapter adapter = new DataHocphiTableAdapters.MonHocTableAdapter();
+            string mamh = "", tenmh = "", sotchp = "";
+           
+            try
+            {
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    mamh = data.Rows[i]["MaMH"].ToString().Trim();
+                    tenmh = data.Rows[i]["TenMH"].ToString().Trim();
+                    sotchp = data.Rows[i]["SoTCHP"].ToString().Trim();
+                   // mamh = data.Rows[i]["MaMH"].ToString().Trim();
+                   
+                    DataHocphi.MonHocDataTable existingEmployee = adapter.GetDataByMaMH(mamh);
+                    // Nếu MaSV chưa tồn tại trong DB thì thêm mới
+                    if (existingEmployee == null || existingEmployee.Rows.Count == 0)
+                    {
+                        adapter.InsertQueryMH(mamh, tenmh, sotchp);
+                    }
+                    // Ngược lại, sinh vien đã tồn tại trong DB thì update
+                    //else
+                    //{
+                    //    adapter.UpdateQueryThuhocphi(hoten, lop, mamh, tenmh, nhhk, sotc, sotchp, dqt, dthi, dtk, dso, diemchu, ngayluu, masv);
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            MessageBox.Show("Kết thúc import");
+        }
+
+        private void frm_Monhoc_Load(object sender, EventArgs e)
+        {
+            Utils.ShowFormCenterOfPanel(this);
+            var rs = mh_ctrl.GetAllMH();
+            UpdateDtg(rs);
+
+        }
+
+        private void btn_capnhat_Click(object sender, EventArgs e)
+        {
+            
+                frm_CapnhatMH frm = new frm_CapnhatMH();
+                frm.txt_mamh.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                frm.txt_tenmh.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                frm.txt_sotchp.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                if (frm.ShowDialog() == DialogResult.OK)
+                {
+                    frm.Close();
+                }
+                var hk = mh_ctrl.GetAllMH();
+                UpdateDtg(hk);
+            
+        }
+        private void UpdateDtg(ActionResult<List<monhoc_ett>> lst)
+        {
+            switch (lst.ErrCode)
+            {
+                case CEnum.HaveNoData:
+                    break;
+                case CEnum.Success:
+                    dataGridView1.DataSource = lst.Data;
+                    break;
+                case CEnum.Fail:
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            frm_ThemMH frm = new frm_ThemMH();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                frm.Close();
+            }
+            var hk = mh_ctrl.GetAllMH();
+            UpdateDtg(hk);
+        }
+        private void txt_timkiem_KeyUp(object sender, KeyEventArgs e)
+        {
+            DataDataContext db = new DataDataContext();
+            var Lst = (from d in db.MonHocs where d.MaMH.Contains(txt_timkiem.Text) select d).ToList();
+            dataGridView1.DataSource = Lst;
+        }
+    }
+}
